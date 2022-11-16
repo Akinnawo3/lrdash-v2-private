0a094a0c5fd51ea6563fe7631caf07f1864b5bd7
MACBOOK <macbook@Vic.local>
2022-11-16 10:44:57 +0100

    FF

--- src/Pages/others/finance/revenue/DailyRevenue.js
+++ src/Pages/others/finance/revenue/DailyRevenue.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Card, CardBody, Col, Row } from "reactstrap";
 import { NairaSuccess } from "../../../../assets/svgs/financeSvgs";
 import { TripsOrange, WatchListSVG1 } from "../../../../assets/svgs/svgs";
+import DailyTripPaymentTable from "./components/DailyTripPaymentTable";
 
 const DailyRevenue = () => {
   return (
@@ -35,9 +36,23 @@ const DailyRevenue = () => {
             </CardBody>
           </Card>
         </Col>
-        {/* <Col className="mt-3" xs={12}>
-          <PerformanceTable />
-        </Col> */}
+        <Col className="mt-3" md={4}>
+          <Card>
+            <CardBody>
+              <div className="d-flex justify-content-between">
+                <TripsOrange />
+                <small className="gradient-text fw-bold">+34%</small>
+              </div>
+              <div>
+                <div className="fs-4">136</div>
+                <small className="text-muted lh-small">No. Of Trips</small>
+              </div>
+            </CardBody>
+          </Card>
+        </Col>
+        <Col className="mt-3" xs={12}>
+          <DailyTripPaymentTable />
+        </Col>
       </Row>
     </div>
   );
